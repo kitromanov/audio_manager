b38863c619f2ccb6cde7365c63585512505893d3
Kit <48387061+kitromanov@users.noreply.github.com>
2023-05-23 01:34:07 +0300

    feat: added models

--- audio_manager/audio/models.py
+++ audio_manager/audio/models.py
@@ -1,3 +1,48 @@
 from django.db import models
+from django.utils.translation import gettext_lazy as _
+from user.models import User
 
-# Create your models here.
+
+class AudioMessage(models.Model):
+    assigned_user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='audio_messages_user')
+    audio_file = models.FileField(upload_to='audio_messages/')
+    audio_url = models.URLField()
+    text = models.TextField(blank=True)
+    duration = models.DurationField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    incoming_number = models.CharField(max_length=20)
+    outgoing_number = models.CharField(max_length=20)
+    is_deleted = models.BooleanField(default=False)
+
+    class Meta:
+        verbose_name = _('audio message')
+        verbose_name_plural = _('audio messages')
+
+    def __str__(self):
+        return self.transcript
+
+
+class Tag(models.Model):
+    name = models.CharField(max_length=30)
+    audio_messages = models.ManyToManyField(AudioMessage, related_name='tags_audio_message')
+
+    class Meta:
+        verbose_name = _('tag')
+        verbose_name_plural = _('tags')
+
+    def __str__(self):
+        return self.name
+
+
+class Comment(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='comments_user')
+    audio_message = models.ForeignKey(AudioMessage, on_delete=models.CASCADE, related_name='comments_audio_message')
+    text = models.TextField()
+
+    class Meta:
+        verbose_name = _('comment')
+        verbose_name_plural = _('comments')
+
+    def __str__(self):
+        return self.text
--- audio_manager/user/models.py
+++ audio_manager/user/models.py
@@ -1,3 +1,49 @@
 from django.db import models
+from django.utils.translation import gettext_lazy as _
+from django.contrib.auth.models import AbstractBaseUser, BaseUserManager, PermissionsMixin
 
-# Create your models here.
+
+class UserManager(BaseUserManager):
+    def _create_user(self, email, username, password, **extra_fields):
+        if not email:
+            raise ValueError("You didn't enter email")
+        if not username:
+            raise ValueError("You didn't enter login")
+        user = self.model(
+            email=self.normalize_email(email),
+            username=username,
+            **extra_fields,
+        )
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_user(self, email, username, password):
+        return self._create_user(email, username, password)
+
+    def create_superuser(self, email, username, password):
+        return self._create_user(email, username, password, is_staff=True, is_superuser=True)
+
+
+class User(AbstractBaseUser, PermissionsMixin):
+    id = models.AutoField(primary_key=True, unique=True)
+    username = models.CharField(max_length=50, unique=True)
+    email = models.EmailField(max_length=100, unique=True)
+    date_joined = models.DateTimeField(auto_now=True)
+
+    # TODO: add email confirmation
+    is_confirmed = models.BooleanField(default=True)
+    is_blocked = models.BooleanField(default=False)
+    is_staff = models.BooleanField(default=False)
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['username']
+
+    objects = UserManager()
+
+    class Meta:
+        verbose_name = _('user')
+        verbose_name_plural = _('users')
+
+    def __str__(self):
+        return self.email
